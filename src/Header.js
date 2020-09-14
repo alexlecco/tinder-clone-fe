a532e368ada35f3051236e91e7f43b9a9dfaf29b
alexlecco <alexlecco24@gmail.com>
2020-09-14 16:14:58 -0300

    complete Header with 3 buttons

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
   return (
@@ -15,6 +16,10 @@ function Header() {
         src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
         alt=""
       />
+
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
     </div>
   );
 }
